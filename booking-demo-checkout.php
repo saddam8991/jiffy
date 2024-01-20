5236ecc49094f596d0b5b5f33441f58c71446953
SDM_111 <157191845+saddam8991@users.noreply.github.com>
2024-01-20 11:08:36 +0530

    Add files via upload

---
+++ booking-demo-checkout.php
@@ -0,0 +1,77 @@
+<br />
+<b>Deprecated</b>:  Return type of Razorpay\Api\Resource::offsetExists($offset) should either be compatible with ArrayAccess::offsetExists(mixed $offset): bool, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/src/Resource.php</b> on line <b>18</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Razorpay\Api\Resource::offsetGet($offset) should either be compatible with ArrayAccess::offsetGet(mixed $offset): mixed, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/src/Resource.php</b> on line <b>28</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Razorpay\Api\Resource::offsetSet($offset, $value) should either be compatible with ArrayAccess::offsetSet(mixed $offset, mixed $value): void, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/src/Resource.php</b> on line <b>23</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Razorpay\Api\Resource::offsetUnset($offset) should either be compatible with ArrayAccess::offsetUnset(mixed $offset): void, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/src/Resource.php</b> on line <b>33</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Razorpay\Api\Resource::getIterator() should either be compatible with IteratorAggregate::getIterator(): Traversable, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/src/Resource.php</b> on line <b>13</b><br />
+<br />
+<b>Deprecated</b>:  Creation of dynamic property Requests_Hooks::$hooks is deprecated in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Hooks.php</b> on line <b>32</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Requests_Cookie_Jar::offsetExists($key) should either be compatible with ArrayAccess::offsetExists(mixed $offset): bool, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Cookie/Jar.php</b> on line <b>52</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Requests_Cookie_Jar::offsetGet($key) should either be compatible with ArrayAccess::offsetGet(mixed $offset): mixed, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Cookie/Jar.php</b> on line <b>62</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Requests_Cookie_Jar::offsetSet($key, $value) should either be compatible with ArrayAccess::offsetSet(mixed $offset, mixed $value): void, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Cookie/Jar.php</b> on line <b>77</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Requests_Cookie_Jar::offsetUnset($key) should either be compatible with ArrayAccess::offsetUnset(mixed $offset): void, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Cookie/Jar.php</b> on line <b>90</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Requests_Cookie_Jar::getIterator() should either be compatible with IteratorAggregate::getIterator(): Traversable, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Cookie/Jar.php</b> on line <b>99</b><br />
+<br />
+<b>Deprecated</b>:  http_build_query(): Passing null to parameter #2 ($numeric_prefix) of type string is deprecated in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Transport/cURL.php</b> on line <b>232</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Requests_Utility_CaseInsensitiveDictionary::offsetExists($key) should either be compatible with ArrayAccess::offsetExists(mixed $offset): bool, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Utility/CaseInsensitiveDictionary.php</b> on line <b>29</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Requests_Utility_CaseInsensitiveDictionary::offsetGet($key) should either be compatible with ArrayAccess::offsetGet(mixed $offset): mixed, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Utility/CaseInsensitiveDictionary.php</b> on line <b>40</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Requests_Utility_CaseInsensitiveDictionary::offsetSet($key, $value) should either be compatible with ArrayAccess::offsetSet(mixed $offset, mixed $value): void, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Utility/CaseInsensitiveDictionary.php</b> on line <b>56</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Requests_Utility_CaseInsensitiveDictionary::offsetUnset($key) should either be compatible with ArrayAccess::offsetUnset(mixed $offset): void, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Utility/CaseInsensitiveDictionary.php</b> on line <b>70</b><br />
+<br />
+<b>Deprecated</b>:  Return type of Requests_Utility_CaseInsensitiveDictionary::getIterator() should either be compatible with IteratorAggregate::getIterator(): Traversable, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in <b>/home4/bytegpmj/www.roarhomz.com/razorpay-php/libs/Requests-1.6.1/library/Requests/Utility/CaseInsensitiveDictionary.php</b> on line <b>79</b><br />
+
+<!-- <button id="rzp-button1">Pay with Razorpay</button> -->
+<script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<form name='razorpayform' action="https://roarhomz.com/booking-demo-payment.php" method="POST">
+    <input type="hidden" name="razorpay_payment_id" id="razorpay_payment_id">
+    <input type="hidden" name="razorpay_signature" id="razorpay_signature">
+</form>
+<script>
+    // Checkout details as a json
+    var options = {"key":"rzp_test_ZqITQjmEGb89M2","amount":150000,"name":"RoarHomz","description":"Rooms On Affordable Rates","image":"https:\/\/www.roarhomz.com\/images\/favicon.png","prefill":{"name":"Test","email":"test@gmail.com","contact":"1234567898"},"notes":{"address":"1","merchant_order_id":"8324"},"theme":{"color":"#F37254"},"order_id":"order_NM7aDIn8cAtyRr"};
+
+    /**
+     * The entire list of Checkout fields is available at
+     * https://docs.razorpay.com/docs/checkout-form#checkout-fields
+     */
+    options.handler = function (response) {
+        document.getElementById('razorpay_payment_id').value = response.razorpay_payment_id;
+        document.getElementById('razorpay_signature').value = response.razorpay_signature;
+        document.razorpayform.submit();
+    };
+
+    // Boolean whether to show image inside a white frame. (default: true)
+    options.theme.image_padding = false;
+
+    options.modal = {
+        ondismiss: function () {
+            console.log("This code runs when the popup is closed");
+        },
+        // Boolean indicating whether pressing escape key 
+        // should close the checkout form. (default: true)
+        escape: true,
+        // Boolean indicating whether clicking translucent blank
+        // space outside checkout form should close the form. (default: false)
+        backdropclose: false
+    };
+
+    var rzp = new Razorpay(options);
+    rzp.open();
+    // document.getElementById('rzp-button1').onclick = function(e){
+    //     rzp.open();
+    //     e.preventDefault();
+    // }
+</script>
